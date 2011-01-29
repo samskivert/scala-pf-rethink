6a67043250d059f1051f1cb5bf20523f09b04b0c
Michael Bayne <mdb@samskivert.com>
2011-01-28 16:21:16 -0800

    Commented out verbose gc and hotspot compilation output for now.

--- project/build/PFRethink.scala
+++ project/build/PFRethink.scala
@@ -1,6 +1,8 @@
 import sbt._
 
 class PFRethink (info :ProjectInfo) extends DefaultProject(info) {
+  // override def fork = forkRun(
+  //   "-Xbatch" :: "-XX:CICompilerCount=1" :: "-XX:+PrintCompilation" :: "-verbosegc" :: Nil)
   override def fork = forkRun(
-    "-Xbatch" :: "-XX:CICompilerCount=1" :: "-XX:+PrintCompilation" :: "-verbosegc" :: Nil)
+    "-Xbatch" :: "-XX:CICompilerCount=1" :: Nil)
 }
